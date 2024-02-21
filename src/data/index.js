a6cdffef9aaec8526c92f49b8df4cf7b4f6568b4
NiyonkuruEsther <estherniyonkuru123@gmail.com>
2024-02-21 13:03:38 +0200

    add carousel

---
+++ src/data/index.js
@@ -0,0 +1,22 @@
+export const Data = [
+  {
+    title: "Enjoy your favorite movie anywhere",
+    description:
+      "Browse through our collections and discover hundreds of movies and series that you'll love!"
+  },
+  {
+    title: "The best of HD that you can't find",
+    description:
+      "Browse through our collections and discover hundreds of movies and series that you'll love!"
+  },
+  {
+    title: "Movies from around the world here",
+    description:
+      "Browse through our collections and discover hundreds of movies and series that you'll love!"
+  },
+  {
+    title: "All the genres of your favorite kind",
+    description:
+      "Browse through our collections and discover hundreds of movies and series that you'll love!"
+  }
+];
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,39 +1,69 @@
 import { View, Text, Dimensions, ImageBackground } from "react-native";
-import React from "react";
+import React, { useRef, useState } from "react";
 import Logo from "../components/Logo";
 import Button from "../components/Button";
+import Carousel from "react-native-snap-carousel";
+import { Data } from "../data";
 
 export const heightFull = Dimensions.get("window").height;
 export const widthFull = Dimensions.get("window").width;
 
 const Home = ({ isLoading }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const carouselRef = useRef(null);
+
+  const horizontalMargin = 20;
+  const slideWidth = 280;
+
+  const sliderWidth = Dimensions.get("window").width;
+  const itemWidth = slideWidth + horizontalMargin * 2;
+  const renderItem = ({ item, index }) => {
+    return (
+      <View className="gap-y-4">
+        <Text className="text-white text-3xl font-bold">{item.title}</Text>
+        <Text className=" text-[19px] text-white">{item.description}</Text>
+      </View>
+    );
+  };
   return !isLoading ? (
     // Home screen
     <ImageBackground
-      source={require("../../assets/jocker2.webp")}
+      source={{
+        uri: "https://www.boredpanda.com/blog/wp-content/uploads/2020/01/3-5e12ff3d9c99c__700.jpg"
+      }}
       resizeMode="cover"
       resizeMethod="scale"
       height={heightFull}
       width={widthFull}
       className="h-[812px]"
     >
-      <View className="bg-bgDarkSecondary flex-1 opacity-70 justify-end px-5" >
+      <View className="bg-bgDarkSecondary flex-1 opacity-70 justify-end px-5">
         <View className=" h-[60%] justify-between pb-10">
           {/* Intro text */}
-          <View className=" gap-y-4">
-            <Text className="text-white text-3xl font-bold">
-              Enjoy your favourite movie anywhere
-            </Text>
-            <Text className=" text-[19px] text-white">
-              Browse through our collections and discover hundreds of movies and
-              series that you'll love!
-            </Text>
-            <View className="flex-row gap-x-2 pt-2">
-              <View className="bg-yellowPrimary h-2 w-14 rounded-full"></View>
-              <View className="bg-grayPrimary h-2 w-6 rounded-full"></View>
-              <View className="bg-grayPrimary h-2 w-6 rounded-full"></View>
+          <View className="gap-y-5">
+            <Carousel
+              ref={carouselRef}
+              layout={"default"}
+              renderItem={renderItem}
+              sliderWidth={sliderWidth}
+              itemWidth={itemWidth}
+              data={Data}
+              onSnapToItem={(index) => setActiveIndex(index)}
+            />
+            <View className="flex-row gap-x-2 pt-2 pl-7">
+              {Data.map((item, index) => (
+                <View
+                  key={index}
+                  className={`bg-${
+                    index === activeIndex ? "yellow" : "gray"
+                  }Primary h-2 w-${
+                    index === activeIndex ? "14" : "6"
+                  } rounded-full`}
+                ></View>
+              ))}
             </View>
           </View>
+
           <Button text="Get started" bgColor={"yellowPrimary"} />
         </View>
       </View>
