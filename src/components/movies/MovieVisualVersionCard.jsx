b26a404b9317caa09bbee76808c0556c4d230e7c
NiyonkuruEsther <estherniyonkuru123@gmail.com>
2024-02-27 09:08:15 +0200

    add

--- src/components/movies/MovieVisualVersionCard.jsx
+++ src/components/movies/MovieVisualVersionCard.jsx
@@ -8,39 +8,37 @@ const MovieVisualVersionCard = ({
   index,
   horizontalDisplay
 }) => {
-  const [isImageLoaded, setIsImageLoaded] = useState(false);
+  const [isImageLoaded, setIsImageLoaded] = useState(true);
 
-  if (!isImageLoaded) {
-    return (
-      <View
-        className={`${imgSize} ${horizontalDisplay && "mr-4"} ${imgSize} ${
-          horizontalDisplay && index === 0 && "ml-5"
-        }`}
-      >
-        <Skeleton height={"auto"} width={"auto"} />
-      </View>
-    );
-  } else {
-    return (
-      <ImageBackground
-        key={index}
-        source={{
-          uri: `https://image.tmdb.org/t/p/original${item.backdrop_path}`
-        }}
-        resizeMode="cover"
-        className={`justify-between ${
-          horizontalDisplay && "mr-4"
-        } ${imgSize} p-3 ${horizontalDisplay && index === 0 && "ml-5"}`}
-        borderRadius={12}
-        onLoadStart={() => setIsImageLoaded(false)}
-        onLoadEnd={() => setIsImageLoaded(true)}
-      >
+  return (
+    <ImageBackground
+      key={index}
+      source={{
+        uri: `https://image.tmdb.org/t/p/original${item.backdrop_path}`
+      }}
+      resizeMode="cover"
+      className={`justify-between ${horizontalDisplay && " mr-4 "} ${imgSize} ${
+        isImageLoaded && "p-3 "
+      } ${horizontalDisplay && index === 0 && "ml-5 "}`}
+      borderRadius={12}
+      onLoadStart={() => setIsImageLoaded(false)}
+      onLoadEnd={() => setIsImageLoaded(true)}
+    >
+      {!isImageLoaded && (
+        <Skeleton
+          height={"auto"}
+          width={"auto"}
+          animation="wave"
+          style={{ borderRadius: 20 }}
+        />
+      )}
+      {isImageLoaded && (
         <View className={`self-end bg-yellowPrimary rounded-md px-2 py-1`}>
           <Text className="font-medium">{item.vote_average.toFixed(1)}</Text>
         </View>
-      </ImageBackground>
-    );
-  }
+      )}
+    </ImageBackground>
+  );
 };
 
 export default MovieVisualVersionCard;
