2d0d5d7e3f40ec6e2696ae411c786abd8911854c
NiyonkuruEsther <estherniyonkuru123@gmail.com>
2024-03-03 17:20:54 +0200

    add flash messages

--- src/screens/Auth/Login.jsx
+++ src/screens/Auth/Login.jsx
@@ -18,8 +18,12 @@ import { heightFull } from "../Home";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { FIREBASE_AUTH } from "../../../FirebaseConfig";
 import { signInWithEmailAndPassword } from "firebase/auth";
+import FlashMessage from "react-native-flash-message";
+import { showMessage, hideMessage } from "react-native-flash-message";
 
 const Login = ({ navigation }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [loginError, setLoginError] = useState("");
   const {
     control,
     handleSubmit,
@@ -45,22 +49,46 @@ const Login = ({ navigation }) => {
 
   const onSubmit = async (data) => {
     try {
-      await signInWithEmailAndPassword(auth,data.email, data.password);
+      await signInWithEmailAndPassword(auth, data.email, data.password);
       reset({
         email: "",
         password: ""
       });
+      setIsLoggedIn(true);
       console.log("logged in successfully");
-      // await HandleGetStore();
     } catch (error) {
-      console.log(error);
+      if (error.code === "auth/too-many-requests") {
+        setLoginError(
+          "Too many request, Please reset Password or Try again later"
+        );
+        console.log(error.code, isLoggedIn);
+      } else if (error.code === "auth/invalid-credential") {
+        setLoginError("Invalid credentials");
+        console.log(error.code, isLoggedIn);
+      } else {
+        console.log(error);
+      }
     }
+    showMessage({
+      message: !isLoggedIn ? loginError : "Logged in successfully",
+      type: !isLoggedIn ? "danger" : "success"
+    });
   };
 
   return (
     <SafeAreaView
       className={`flex-1 bg-bgDarkPrimary px-[16px] pt-3 pb-5 h-[${heightFull}px]`}
     >
+      <FlashMessage
+        duration={3000}
+        position={"top"}
+        message={
+          !isLoggedIn && loginError
+            ? { message: loginError }
+            : { message: "Logged in successfully" }
+        }
+        textStyle={{ textAlign: "center" }}
+      />
       <KeyboardAvoidingView behavior="position" className={`flex-1`}>
         <View className={`justify-between h-full `}>
           <View className="flex-row gap-5 items-center">
